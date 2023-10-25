c06017008cbc4ba39a6d577ea33ed3d59e2a29ee
Mayce <maynab4@gmail.com>
2023-10-25 06:49:21 -0700

    Adding workshops page

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link"
-// import Image from "next/image"
+import Link from "next/link";
+import Image from "next/image"
 // import { useTranslation } from "../i18n"
 // import HomeBg from "../../public/assets/images/homeBg.jpg";
 // import Button from "../../public/assets/images/button.png";
@@ -11,21 +11,32 @@ import Link from "next/link"
 export default async function page() {
   // const { t } = await useTranslation(lng, "translations")
   return (
-    <div className="">
-      <div className="relative z-0 bg-cover bg-center h-[1261px]" style={{ backgroundImage: "url('/assets/images/homeBg.jpg')" }}>
+    <section className="h-screen bg-black">
+      <div
+        className="relative z-0 bg-cover bg-center h-[1261px]"
+        style={{ backgroundImage: "url('/assets/images/homeBg.png')" }}
+      >
         {/* Content goes herefddgfdg */}
-        <Link href="/second-page" className="absolute top-[425px] right-[70px] text-3xl text-white z-3">يلا !</Link>
-
+        <Link
+          href="/second-page"
+          className="absolute top-[425px] right-[70px] text-3xl text-white z-3"
+        >
+          يلا !
+        </Link>
       </div>
-    </div>
 
-    // <div className="homeBg" >
-    //   {/* <Image src={HomeBg} alt=""  /> */}
-    //   <Image src={Button} alt="" className="absolute"/>
-    //   {/* <h1>{t('working')}</h1>
-    //   <Link href={`/${lng}/second-page`}>
-    //   {t('back-to-home')}
-    //   </Link> */}
-    // </div>
-  )
+      {/* Content goes herefddgfdg */}
+      <Link
+        href="/workshops"
+        className="h-full w-full bg-white"
+      >
+        <div className="absolute top-[580px] bg-transparent w-60 right-[30px] h-60 text-3xl text-white z-3">
+       sad
+        </div>
+
+      </Link>
+    
+
+    </section>
+  );
 }
