29e83d1cf8ebf6482ffb9fe1fcc23aec0c65c890
Mayce <maynab4@gmail.com>
2023-10-26 09:23:24 -0700

    Adding md display

--- app/[lng]/workshops/page.tsx
+++ app/[lng]/workshops/page.tsx
@@ -4,21 +4,20 @@ export default function s() {
   return (
 
     <div className='h-screen bg-[#FCD2D1]'>
-      <div className="top relative h-2/5 pt-10">
-        <div className="left relative ontheback h-full w-3/3 z-0  ">
-          <Image src="/assets/svg/handBuilding1.svg" height={363} width={225} alt="" className='absolute' />
+      <div className="top relative h-2/5 pt-10 sm:hidden">
+        <div className="left relative ontheback h-full w-3/3 z-0">
+          <Image src="/assets/svg/handBuilding1.svg" height={363} width={225} alt="" className='absolute sm:right-[100px]' />
         </div>
-        <div className="right onthetop z-10
-         absolute bg-transparent pt-12 pr-6 h-full w-1/2 top-0 left-0">
+        <div className="right onthetop z-10 absolute bg-transparent pt-12 pr-6 h-full w-1/2 top-0 left-0">
           <Image src="/assets/svg/byHand.svg" height={500} width={100} alt="" className='' />
           <Image src="/assets/svg/text1.svg" height={172} width={152} alt="" className='mt-5' />
           <RegisterHere />
         </div>
       </div>
-      <div className="divider relative h-1/5 ">
+      <div className="divider relative h-1/5 sm:hidden">
         <Image src="/assets/svg/vasesLine.svg" height={100} width={10} alt="" className='w-full h-full' />
       </div>
-      <div className="bottom relative h-2/5">
+      <div className="bottom relative h-2/5 sm:hidden">
         <div className="right onthetop z-10
          absolute bg-transparent pr-6 h-full w-1/2 top-0 pt-4">
           <Image src="/assets/svg/wheelBuilding.svg" height={500} width={100} alt="" className='' />
@@ -28,6 +27,43 @@ export default function s() {
         <div className="left relative ontheback h-full w-3/3 z-0">
           <Image src="/assets/svg/wheel.svg" height={453} width={232} alt="" className='absolute h-full left-0 w-2/3' />
         </div>
+      </div>
+
+      {/* md */}
+      <div className="hidden sm:flex justify-between h-screen">
+       <div className="one flex flex-col w-2/5 justify-around items-center h-full ">
+        <div className="a w-full relative right-[45%]">
+        <Image src="/assets/svg/md/handBuilding.svg" height={193} width={262} alt="" />
+        <div className="absolute top-[270px] right-[-37px]">
+        <Image src="/assets/svg/md/byHand.svg" height={193} width={110} alt="w-full h-full " />
+        <div className="mt-4">
+        <Image src="/assets/svg/md/text1.svg" height={571} width={301} alt="w-full h-full mt-[4px]" />
+        </div>
+        </div>
+        </div>
+        
+        <div className="a bottom-0  ">
+         dsfd
+        </div>
+       </div>
+        <div className="divider h-full">
+          <Image src="/assets/svg/md/divider.svg" height={1002} width={10} alt="" className='h-full' />
+        </div>
+        <div className="one flex flex-col w-2/5 justify-around items-center h-full ">
+        <div className="a w-full relative">
+        <Image src="/assets/svg/md/wheel.svg" height={508} width={262} alt="" className='w-[400px] right-[-10px]' />
+        <div className="absolute top-[230px] right-[37px]">
+        <Image src="/assets/svg/md/buildingWheel.svg" height={78} width={145} alt="w-full h-full " />
+        <div className="mt-4">
+        <Image src="/assets/svg/md/text2.svg" height={571} width={301} alt="w-full h-full mt-[4px]" />
+        </div>
+        </div>
+        </div>
+        
+        <div className="a bottom-0  ">
+         dsfd
+        </div>
+       </div>
       </div>
     </div>
 
--- components/RegisterHere.tsx
+++ components/RegisterHere.tsx
@@ -5,9 +5,13 @@ import Link from "next/link"
 export default function RegisterHere() {
   return (
     <Link href="https://tally.so/r/wQRJ5G" target="_blank">
-      <div className="btn2 relative bg-transparent top-[-20px] right-[-30px] ">
+      <div className="btn2 relative sm:hidden bg-transparent top-[-20px] right-[-30px] ">
         <Image src="/assets/svg/btn2.svg" height={1000} width={0} alt="" className='w-full bg-transparent h-full z-0' />
         <Image src="/assets/svg/registerHere.svg" height={1} width={1} alt="" className='absolute z-3 bottom-6 right-12 h-1/2 w-1/2 ' />
+      </div>
+      <div className="btn2 hidden sm:block bg-transparent top-[-20px] right-[-30px] ">
+        <Image src="/assets/svg/btn2.svg" height={10} width={0} alt="" className='w-full bg-transparent h-full z-0' />
+        <Image src="/assets/svg/registerHere.svg" height={1} width={1} alt="" className='absolute z-3 bottom-6 right-12 h-1/2 w-1/2' />
       </div>
     </Link>
   )
